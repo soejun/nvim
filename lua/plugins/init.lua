0ec817fc718aa495d21b45de3fc7d5bb060e63ee
soejun <chan.wilson.wc@gmail.com>
2023-04-13 19:05:00 -0700

    feat: init.lua change

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,6 @@
 ---@diagnostic disable: different-requires
 -- TODO: Seriously, we need a way better way to organize this and make it maintainable jesus christ
+-- we can start categorizng thngs, into ui, lsp, quality of life
 
 local default_plugins = {
   "nvim-lua/plenary.nvim",
