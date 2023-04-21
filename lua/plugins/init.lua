7005914a02495f4a3eb2ff5c83d8cb8ab8e69f8d
soejun <chan.wilson.wc@gmail.com>
2023-04-21 14:36:45 -0700

    bug: fixed typo for dap signs

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -401,7 +401,7 @@ local default_plugins = {
       local icons = require("utils.lazyvim-icons")
       for name, sign in pairs(icons.dap) do
         sign = type(sign) == "table" and sign or { sign }
-        vim.fn.sign_defin(
+        vim.fn.sign_define(
           "Dap" .. name,
           { text = sign[1], texthl = sign[2] or "DiagnosticInfo", linehl = sign[3], numhl = sign[3] }
         )
