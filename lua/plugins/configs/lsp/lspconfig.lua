bba07f676450bf7975510530220b05ad34e112e1
soejun <chan.wilson.wc@gmail.com>
2023-07-24 10:57:08 -0700

    fix: removed the questions marks

--- lua/plugins/configs/lsp/lspconfig.lua
+++ lua/plugins/configs/lsp/lspconfig.lua
@@ -27,16 +27,15 @@ M.on_attach = function(client, bufnr)
   end
 
   local border = {
-    -- { "🭽", "FloatBorder" },
-    { "▔", "FloatBorder" },
-    -- { "🭾", "FloatBorder" },
-    { "▕", "FloatBorder" },
-    -- { "🭿", "FloatBorder" },
-    { "▁", "FloatBorder" },
-    -- { "🭼", "FloatBorder" },
-    { "▏", "FloatBorder" },
+    { "─", "FloatBorder" },
+    { "│", "FloatBorder" },
+    { "─", "FloatBorder" },
+    { "│", "FloatBorder" },
+    { "┌", "FloatBorder" },
+    { "┐", "FloatBorder" },
+    { "┘", "FloatBorder" },
+    { "└", "FloatBorder" },
   }
-
   vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = border })
   vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border })
   -- if client.supports_method("textDocument/formatting") then
