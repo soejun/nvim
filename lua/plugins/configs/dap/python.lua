20f297bd3f074bcaf66fe1ceb7c164ecd7066e1f
soejun <chan.wilson.wc@gmail.com>
2024-02-06 11:30:25 -0500

    dap-python for pyenv

--- lua/plugins/configs/dap/python.lua
+++ lua/plugins/configs/dap/python.lua
@@ -1,7 +1,11 @@
 local M = {}
+-- The debugger will automatically pick-up another virtual environment if it is activated before neovim is started.
 
 function M.setup(_)
-  require("dap-python").setup("~/.virtualenvs/debugpy/bin/python")
+  -- require("dap-python").setup("~/.virtualenvs/debugpy/bin/python")
+  require("dap-python").resolve_python = function()
+    return "/home/wchan/.pyenv/shims/python3"
+  end
 end
 
 return M
