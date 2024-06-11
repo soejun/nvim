b466e925af7a4241df70bec1ba5ff771926df9b1
soejun <chan.wilson.wc@gmail.com>
2024-06-11 14:41:43 -0400

    disable cursor for windows
    fix

--- init.lua
+++ init.lua
@@ -31,7 +31,9 @@ vim.opt.rtp:prepend({ lazypath })
 
 require("plugins") --plugin management via lazy
 require("core.autocmd") -- vim autocommands/autogroups
+if is_windows == false then
 vim.cmd([[  set guicursor=n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50
   \,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor
   \,sm:block-blinkwait175-blinkoff150-blinkon175]])
+end
 AUTOFORMAT_ACTIVE = false
