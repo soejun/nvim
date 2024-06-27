58f548ab81bbb2236f2756823a0901ddc0e1cb88
soejun <chan.wilson.wc@gmail.com>
2024-06-27 13:47:23 -0400

    update options

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,4 +1,3 @@
--- all vim-related options, non lua
 local opt = vim.opt
 local g = vim.g
 local settings = require("core.settings")
@@ -17,46 +16,62 @@ else
   opt.laststatus = 2
 end
 
--- winbar
 vim.opt.winbar = "%{%v:lua.require'utils.winbar'.get_winbar()%}"
 
-opt.cursorline = true
+
+vim.g.deprecation_warnings = false
+
+
+opt.completeopt = "menu,menuone,noselect"
+opt.conceallevel = 2 -- Hide * markup for bold and italic, but not markers with substitutions
+
 
 -- Indenting
 opt.expandtab = true
+opt.shiftround = true -- Round indent
 opt.shiftwidth = 2
 opt.smartindent = true
 opt.tabstop = 2
 opt.softtabstop = 2
+opt.list = true -- show some invisible characters (tabs..)
 
 -- nvim-ufo folding
 opt.foldcolumn = "1"
 opt.foldlevel = 99
 opt.foldlevelstart = -1
 opt.foldenable = true
-opt.fillchars = { eob = " ", fold = " ", foldopen = " ", foldsep = " ", foldclose = " " }
+opt.fillchars = {
+  foldopen = "",
+  foldclose = "",
+  fold = " ",
+  foldsep = " ",
+  diff = "╱",
+  eob = " ",
+}
+
 --
+opt.shortmess:append("sI") -- disable nvim intro
+opt.linebreak = true -- Wrap lines at convenient points
+opt.cursorline = true
+opt.mouse = "a"
+opt.inccommand = "nosplit" -- preview incremental substitute
 opt.ignorecase = true
 opt.smartcase = true
-opt.mouse = "a"
-
--- Numbers
 opt.number = true
 opt.numberwidth = 2
 opt.ruler = false
-
--- disable nvim intro
-opt.shortmess:append("sI")
-
-opt.signcolumn = "yes"
+opt.wrap = false -- disable line wrap
+opt.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
+opt.sidescrolloff = 8 -- Columns of context
 opt.splitbelow = true
 opt.splitright = true
 opt.termguicolors = true
 opt.timeoutlen = 400
 opt.undofile = true
-
--- interval for writing swap file to disk, also used by gitsigns
-opt.updatetime = 25
+opt.undolevels = 10000
+opt.virtualedit = "block" -- Allow cursor to move where there is no text in visual block mode
+opt.wildmode = "longest:full,full" -- Command-line completion mode
+opt.updatetime = 200 -- interval for writing swap file to disk, also used by gitsigns
 
 -- go to previous/next line with h,l,left arrow and right arrow
 -- when cursor reaches end/beginning of line
@@ -77,3 +92,6 @@ vim.o.cmdheight = 1
 vim.o.title = true
 vim.o.titlestring = "nvim/" .. vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
 vim.o.titleold = "Terminal"
+
+-- Fix markdown indentation settings
+vim.g.markdown_recommended_style = 0
