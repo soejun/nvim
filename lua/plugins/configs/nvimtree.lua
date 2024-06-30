c2a7ca9b7eee0b34b7f83d4789e9f3cf3fd4a447
soejun <chan.wilson.wc@gmail.com>
2024-06-30 16:18:08 -0400

    update nvim-tree for adaptive sizing

--- lua/plugins/configs/nvimtree.lua
+++ lua/plugins/configs/nvimtree.lua
@@ -15,7 +15,7 @@ local options = {
     update_root = false,
   },
   view = {
-    adaptive_size = false,
+    adaptive_size = true,
     side = "right",
     width = 30,
     preserve_window_proportions = true,
