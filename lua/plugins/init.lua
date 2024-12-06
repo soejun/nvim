e93721734855c992f180c83af1d6ef73afbc6a0f
soejun <chan.wilson.wc@gmail.com>
2024-12-06 11:08:06 -0500

    Adds aerial.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -588,7 +588,38 @@ local default_plugins = {
       require("outline").setup(opts)
     end,
   },
-
+  {
+    "stevearc/aerial.nvim",
+    opts = function()
+      local filter_kind = false
+      local opts = {
+        attach_mode = "global",
+        backends = { "lsp", "treesitter", "markdown", "man" },
+        show_guides = true,
+        layout = {
+          default_direction = "prefer_left",
+          resize_to_content = false,
+          win_opts = {
+            winhl = "Normal:NormalFloat,FloatBorder:NormalFloat,SignColumn:SignColumnSB",
+            signcolumn = "yes",
+            statuscolumn = " ",
+          },
+        },
+        filter_kind = filter_kind,
+      -- stylua: ignore
+      guides = {
+        mid_item   = "├╴",
+        last_item  = "└╴",
+        nested_top = "│ ",
+        whitespace = "  ",
+      },
+      }
+      return opts
+    end,
+    keys = {
+      { "<leader>cs", "<cmd>AerialToggle<cr>", desc = "Aerial (Symbols)" },
+    },
+  },
   {
     "luukvbaal/statuscol.nvim",
     lazy = false,
