e3e30dfb4cf7175966a0adcdce09372b9989c7b0
soejun <chan.wilson.wc@gmail.com>
2025-04-24 11:35:11 -0400

    add note to downgrade lsps in mason registry

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,5 +1,8 @@
 return {
   "williamboman/mason.nvim",
+  -- https://github.com/williamboman/mason.nvim/issues/1508#issuecomment-1745259185
+  -- To downgrade a lsp, you'd have to go into ~/.local/share/nvim/mason/registries/github/mason-org/mason-registry
+  -- and  manually change the  target
   opts = {
     ensure_installed = {
       "stylua",
