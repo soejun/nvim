007b002aa6dd8329e398b8243283cc83efb5c50a
soejun <chan.wilson.wc@gmail.com>
2025-05-09 16:39:12 -0400

    update snacks bindings

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -19,17 +19,17 @@ return {
   keys = {
     -- Note: Disable for No-Neck-Pain, use <leader>snt instead
     -- Disable keymap to pick snacks notifcations
-    {"<leader>n", false}
+    {"<leader>n", false},
       -- { "<leader>n", function() Snacks.picker.notifications() end, desc = "Notification History" },
 
     -- TODO: Until we can make consistent disable our previous binding switch
     -- LazyVim docs imo switch the meaning of root dir and CWD, at least in a working sense,
     -- this is why we swap the =biindings here and set root = false
     -- { "<leader>/", LazyVim.pick("grep", { root = false }), desc = "Grep (cwd)" },
-    -- { "<leader><space>", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
+    { "<leader><space>", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
     -- find
-    -- { "<leader>fF", LazyVim.pick("files"), desc = "Find Files (Root Dir)" },
-    -- { "<leader>ff", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
+    { "<leader>fF", LazyVim.pick("files"), desc = "Find Files (Root Dir)" },
+    { "<leader>ff", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
     -- grep
     -- { "<leader>sG", LazyVim.pick("live_grep"), desc = "Grep (Root Dir)" },
     -- { "<leader>sg", LazyVim.pick("live_grep", { root = false }), desc = "Grep (cwd)" },
