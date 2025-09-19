0e0a816c5700be96ef57bf0be5b493daa0130012
soejun <chan.wilson.wc@gmail.com>
2025-09-19 13:44:24 -0400

    Update to LazyVim 15.1 OUR GOAT IS BACK HES BACK

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,5 +1,5 @@
 return {
-  "williamboman/mason.nvim",
+  "mason-org/mason.nvim",
   -- https://github.com/williamboman/mason.nvim/issues/1508#issuecomment-1745259185
   -- To downgrade a lsp, you'd have to go into ~/.local/share/nvim/mason/registries/github/mason-org/mason-registry
   -- and  manually change the  target
--- lua/plugins/mini-indentscope.lua
+++ lua/plugins/mini-indentscope.lua
@@ -1,5 +1,5 @@
 return {
-  "echasnovski/mini.indentscope",
+  "nvim-mini/mini.indentscope",
   opts = {
     draw = {
       delay = 50,
