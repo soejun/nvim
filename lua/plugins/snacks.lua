0625806f807bbf58556d38492a83c65c03cf8da5
soejun <chan.wilson.wc@gmail.com>
2025-10-31 12:07:36 -0400

    toggle ignore and hidden for snacks picker by default

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -12,13 +12,13 @@ return {
         },
       },
     },
-  },
-  picker = {
-    sources = {
-      explorer = {},
+    picker = {
+      sources = {
+        explorer = {},
+      },
     },
   },
-  -- URL for default bindings: https://www.lazyvim.org/extras/editor/snacks_picker
+-- URL for default bindings: https://www.lazyvim.org/extras/editor/snacks_picker
   -- stylua: ignore
   keys = {
     -- Note: Disable for No-Neck-Pain, use <leader>snt instead
@@ -30,17 +30,16 @@ return {
     -- LazyVim docs imo switch the meaning of root dir and CWD, at least in a working sense,
     -- this is why we swap the =biindings here and set root = false
     -- { "<leader>/", LazyVim.pick("grep", { root = false }), desc = "Grep (cwd)" },
-    { "<leader><space>", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
+    { "<leader><space>", LazyVim.pick("files", { root = false, hidden=true, ignored=true }), desc = "Find Files (cwd)" },
     -- find
     -- { "<leader>fF", LazyVim.pick("files"), desc = "Find Files (Root Dir)" },
     -- { "<leader>ff", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
     -- grep
     -- { "<leader>sG", LazyVim.pick("live_grep"), desc = "Grep (Root Dir)" },
     -- { "<leader>sG", LazyVim.pick("live_grep"), desc = "Grep (Root Dir)" },
-    { "<leader>/", LazyVim.pick("live_grep", { root = false }), desc = "Grep (cwd)" },
+    { "<leader>/", LazyVim.pick("live_grep", { root = false, hidden=true, ignored=true}), desc = "Grep (cwd)" },
     -- { "<leader>sW", LazyVim.pick("grep_word"), desc = "Visual selection or word (Root Dir)", mode = { "n", "x" } },
     -- { "<leader>sw", LazyVim.pick("grep_word", { root = false }), desc = "Visual selection or word (cwd)", mode = { "n", "x" } },
-    -- search
   }
 ,
 }
