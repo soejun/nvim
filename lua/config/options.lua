bbc4518340b500a0ca4f042a4b743e7ef1a98874
soejun <chan.wilson.wc@gmail.com>
2026-07-14 11:14:24 -0400

    debugging lazyvim

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,9 +4,26 @@
 
 vim.g.autoformat = false
 vim.g.snacks_animate = true
--- vim.g.lazyvim_python_lsp = "jedi_language_server"
 vim.g.lazyvim_python_lsp = "basedpyright"
--- vim.opt.shiftwidth = 4
--- vim.opt.tabstop = 4
--- vim.opt.linebreak = true
--- vim.opt.wrap = true
+
+vim.api.nvim_create_user_command("LspInfo", ":checkhealth vim.lsp", { desc = "Alias to `:checkhealth vim.lsp`" })
+
+-- Show the opts lazy.nvim hands a plugin after LazyVim defaults, extras and
+-- lua/plugins/* are deep-merged, e.g. :LazyOpts nvim-lspconfig
+vim.api.nvim_create_user_command("LazyOpts", function(cmd)
+  local plugin = require("lazy.core.config").plugins[cmd.args]
+  if not plugin then
+    return vim.notify("No plugin named `" .. cmd.args .. "`", vim.log.levels.ERROR)
+  end
+  Snacks.debug.inspect(require("lazy.core.plugin").values(plugin, "opts", false))
+end, {
+  nargs = 1,
+  desc = "Inspect a plugin's merged opts",
+  complete = function(prefix)
+    local names = vim.tbl_filter(function(name)
+      return name:find(prefix, 1, true) == 1
+    end, vim.tbl_keys(require("lazy.core.config").plugins))
+    table.sort(names)
+    return names
+  end,
+})
